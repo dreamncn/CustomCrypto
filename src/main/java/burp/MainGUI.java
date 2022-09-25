bd7de04aa1efd4b178246311004822ebdb173d9c
ankio <dream@dreamn.cn>
2022-09-25 19:33:01 +0800

    fix: 移出jForm添加的logo

--- src/main/java/burp/MainGUI.java
+++ src/main/java/burp/MainGUI.java
@@ -675,7 +675,7 @@ public class MainGUI extends JPanel {
             //======== watch ========
             {
                 watch.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax
-                . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax. swing
+                . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "" , javax. swing
                 .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .
                 Font ( "D\u0069alog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red
                 ) ,watch. getBorder () ) ); watch. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override
@@ -1078,7 +1078,7 @@ public class MainGUI extends JPanel {
                 //======== panel7 ========
                 {
                     panel7.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder( 0
-                    , 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM
+                    , 0, 0, 0) , "", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM
                     , new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ), java. awt. Color. red) ,
                     panel7. getBorder( )) ); panel7. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e
                     ) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
