98afaf74b5b6c29ef03ef4effe01934503c540c5
ankio <dream@dreamn.cn>
2022-09-26 22:18:49 +0800

    fix: 删除JForm logo

--- src/main/java/burp/MainGUI.java
+++ src/main/java/burp/MainGUI.java
@@ -543,7 +543,7 @@ public class MainGUI extends JPanel {
             //======== watch ========
             {
                 watch.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder(
-                0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder
+                0, 0, 0, 0) , "", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder
                 . BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color.
                 red) ,watch. getBorder( )) ); watch. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .
                 beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
@@ -855,7 +855,7 @@ public class MainGUI extends JPanel {
                 //======== panel7 ========
                 {
                     panel7.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new
-                    javax. swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax
+                    javax. swing. border. EmptyBorder( 0, 0, 0, 0) , "", javax
                     . swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java
                     .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt
                     . Color. red) ,panel7. getBorder( )) ); panel7. addPropertyChangeListener (new java. beans.
